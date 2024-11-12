d4bbc0e0f54042851e573492a840cdb84938080b
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-11-12 21:09:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let timerDisplay = document.getElementById("timer");
+
+function updateTimer() {
+  let currentDate = new Date();
+  timerDisplay.innerHTML = currentDate.toLocaleString();
+}
+
+
+setInterval(updateTimer, 1000);
+updateTimer(); 
